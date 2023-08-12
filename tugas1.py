acea37807b5c2f6c2aeade89c953334204d7a372
ADHI DEV <139231935+ADHI-IT@users.noreply.github.com>
2023-08-12 07:51:25 +0700

    Add files via upload

---
+++ tugas1.py
@@ -0,0 +1,3 @@
+print("NAMA: ADHIPRAMANA PUTRA")
+print("KELAS: X RPL-1")
+print("Saya di rpl ini ingin menjadi web apps fullstack dan mobile apps developer, dan karena saya suka dibagian komputer seperti mengoding, mengotak atik software, dan juga karena tujuan saya ada disini.")
